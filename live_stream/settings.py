5d13ed9f2965a72e129b5f17153098ea0ebd4e77
radwanaltaf <radwanaltaf2000@gmail.com>
2021-10-26 14:15:39 +0800

    staticfiles root dir for prod env

--- live_stream/settings.py
+++ live_stream/settings.py
@@ -135,7 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "/DjangoLiveStream/site/public/static"
 STATIC_URL = '/static/'
 
 # Default primary key field type
