a3196510b6555fee2c52b5faeba198fb143ad712
radwanaltaf <radwanaltaf2000@gmail.com>
2021-10-26 13:07:19 +0800

    db change to mysql for prod

--- live_stream/settings.py
+++ live_stream/settings.py
@@ -86,12 +86,16 @@ WSGI_APPLICATION = 'live_stream.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
             'read_default_file': '/DjangoLiveStream/auth/mysql.cnf'
         },
-    }
+    }    
 }
 
 
